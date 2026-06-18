0a293020ab2735027fb7d2812fc45ae89dce40fe
haezaaofficial-codemy <haezaaofficial@gmail.com>
2026-06-18 07:30:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CodeMy</title>
+</head>
+<body>
+    <?php include "layout/header.html" ?>
+    <main></main>
+        <p>CodeMy adalah platform pembelajaran online yang menyediakan berbagai materi tentang koding dan terminologi medis. Kami bertujuan untuk membantu Anda meningkatkan keterampilan coding dan pemahaman tentang istilah medis dengan cara yang menyenangkan dan interaktif.</p>
+        <p>Mulailah perjalanan belajar Anda dengan mendaftar sekarang dan jelajahi berbagai kursus yang kami tawarkan!</p>
+    </main>
+    <?php include "layout/footer.html" ?>
+</body>
+</html>
